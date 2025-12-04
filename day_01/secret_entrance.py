257622d384d6f8d5ac22c045c4d3c163900f3a68
Joshua LaBelle <labelljd@gmail.com>
2025-12-03 22:03:44 -0500

    initial commit w/ day 1 tests complete

---
+++ day_01/secret_entrance.py
@@ -0,0 +1,11 @@
+
+def dial(rotation):
+    pass
+
+def main():
+    with open("input.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end='')
+
+if __name__ == "__main__":
+    main()
