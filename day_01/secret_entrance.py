d41118779e942b19008e4f17593dccd5610c067a
Joshua LaBelle <labelljd@gmail.com>
2025-12-03 23:30:33 -0500

    wip, start fleshing out logic

--- day_01/secret_entrance.py
+++ day_01/secret_entrance.py
@@ -1,6 +1,23 @@
 
 def dial(rotation):
-    pass
+    zero_count = 0
+    arrow = 50
+    direction, num = rotation[0], int(rotation[1:])
+    if direction == 'L':
+        arrow = arrow - num
+
+    # There is probably a better way to do this that the constant type conversion. First pass though.
+    if direction == 'R':
+        if arrow + num > 99:
+            sum = str(arrow + num)
+            arrow = int(sum[-2:])
+            if arrow == 0:
+                zero_count += 1
+        else:
+            arrow = arrow + num
+            if arrow == 0:
+                zero_count += 1
+
 
 def main():
     with open("input.txt", "r", encoding="utf-8") as f:
