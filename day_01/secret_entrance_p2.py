d5acf2c306196c3845b7e927888a0edb51f61038
Joshua LaBelle <labelljd@gmail.com>
2025-12-06 18:08:20 -0500

    finish day 1 part 2 w/ updated tests

---
+++ day_01/secret_entrance_p2.py
@@ -0,0 +1,45 @@
+# day 1 part 2 SUCCESSFUL solution
+
+def dial2(arrow, rotation):
+    """
+    50 - 68 --> 82    0 - 5 --> 95
+    Dial values represented by `range(100)`
+    """
+    direction, num = rotation[0], int(rotation[1:])
+    zeros = 0
+    # Deal with cases where num is > 100 and account for extra zero passes
+    if num not in range(100):
+        zeros += (num // 100)
+        num = int(str(num)[-2:])
+
+    if direction == 'L':
+        # Don't count zero passes when the arrow starts at 0
+        if num > arrow and arrow != 0:
+            zeros += 1
+        arrow = range(100)[arrow - num]
+
+    if direction == 'R':
+        if arrow + num > 100:
+            arrow = (arrow + num) - 100
+            zeros += 1
+        elif arrow + num == 100:
+            arrow = 0
+        else:
+            arrow = arrow + num
+
+    return zeros, arrow
+
+def main():
+    zero_count = 0
+    arrow = 50
+    with open("input.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            #print(line, end='')
+            zeros, arrow = dial2(arrow, line)
+            zero_count += zeros
+            if arrow == 0:
+                zero_count += 1
+    print(zero_count)
+
+if __name__ == "__main__":
+    main()
--- day_01/test_dial.py
+++ day_01/test_dial.py
@@ -1,13 +1,14 @@
 import pytest
 from secret_entrance import dial
+from secret_entrance_p2 import dial2
 
-def test_single_result():
+def test_p1_single_result():
     arrow = 95
     rot = "R60"
     result = dial(arrow, rot)
     assert result == 55
 
-def test_results():
+def test_p1_results():
     rotations = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
     expected = ["82", "52", "0", "95", "55", "0", "99", "0", "14", "32"]
 
@@ -20,3 +21,31 @@ def test_results():
 
     for index, result in enumerate(results):
         assert result == int(expected[index]), f"Test case fails, got: {result}, expected: {expected[index]}"
+
+def test_p1_zeros():
+    rotations = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
+    expected_zeros = 3
+    zero_count = 0
+    arrow = 50
+
+    for rotation in rotations:
+        arrow = dial(arrow, rotation)
+        if arrow == 0:
+            zero_count += 1
+
+    assert zero_count == expected_zeros
+
+def test_p2_zeros():
+    rotations = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
+    expected_zeros = 6
+    zero_count = 0
+    arrow = 50
+
+    for rotation in rotations:
+        zeros, arrow = dial2(arrow, rotation)
+        zero_count += zeros
+        if arrow == 0:
+            zero_count += 1
+        print(rotation, zero_count)
+
+    assert zero_count == expected_zeros
