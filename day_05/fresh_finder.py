c9a1b8cd0abf07f1ea7524250fd4617ed0ec48c4
Joshua LaBelle <labelljd@gmail.com>
2025-12-31 16:40:18 -0500

    solve day5p1

--- day_05/fresh_finder.py
+++ day_05/fresh_finder.py
@@ -1,24 +1,33 @@
-# Day 5 Part 1
+# Day 5 Part 1 Successful Solution 
 import re
 
-def find_fresh(range_lst: list[str], ingredients: list[str]) -> int:
-
-    return 1
+def find_fresh(range_lst: list[tuple], ingredients: list[int]) -> int:
+    """Find the fresh ingredients"""
+    fresh_set = set()
+    for ingredient in ingredients:
+        for rng in range_lst:
+            start, end = rng
+            if ingredient in range(start, end + 1):
+                fresh_set.add(ingredient)
+                
+    return len(fresh_set)
 
 def process_input(input_file):
     """
     Parse input file to capture fresh ingredient ranges and available
-    ingredients into two separate lists and return them.
+    ingredients into two separate lists and return them. The ranges are
+    converted into tuples for easy processing in find_fresh.
     """
     range_lst = []
     ingredients = []
 
     with open(input_file, 'r', encoding='utf-8') as f:
         for line in f:
-            if re.match(r"^\d+[-]\d+$", line):
-                range_lst.append(line.strip())
+            match = re.match(r"^(\d+)-(\d+)$", line)
+            if match:
+                range_lst.append((int(match.group(1)), int(match.group(2))))
             elif re.match(r"^\d+$", line):
-                ingredients.append(line.strip())
+                ingredients.append(int(line.strip()))
 
     # print(range_lst)
     # print("--------BREAK---------")
@@ -27,7 +36,10 @@ def process_input(input_file):
     return range_lst, ingredients
 
 def main():
-    process_input("test_input.txt")
+    ranges, items = process_input("input.txt")
+    result = find_fresh(ranges, items)
+    print(result)
+
 
 if __name__ == '__main__':
     main()
--- day_05/test_fresh_finder.py
+++ day_05/test_fresh_finder.py
@@ -5,12 +5,12 @@ from fresh_finder import process_input
 def test_process_input():
     range_lst, ingred_lst = process_input("test_input.txt")
 
-    assert range_lst == ["3-5", "10-14", "16-20", "12-18"]
-    assert ingred_lst == ["1", "5", "8", "11", "17", "32"]
+    assert range_lst == [(3, 5), (10, 14), (16, 20), (12, 18)]
+    assert ingred_lst == [1, 5, 8, 11, 17, 32]
 
 def test_find_fresh():
-    ingredient_ranges = ["3-5", "10-14", "16-20", "12-18"]
-    ingredients = ["1", "5", "8", "11", "17", "32"]
+    ingredient_ranges = [(3, 5), (10, 14), (16, 20), (12, 18)]
+    ingredients = [1, 5, 8, 11, 17, 32]
 
     expected_fresh_ingredients = 3
     actual_fresh_ingredients = find_fresh(ingredient_ranges, ingredients)
