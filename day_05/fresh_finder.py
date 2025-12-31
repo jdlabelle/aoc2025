75b73d00ab3b51dd87029c99d2afe74b46a0d053
Joshua LaBelle <labelljd@gmail.com>
2025-12-30 23:01:07 -0500

    progress on day5p1

--- day_05/fresh_finder.py
+++ day_05/fresh_finder.py
@@ -1,14 +1,33 @@
 # Day 5 Part 1
+import re
 
 def find_fresh(range_lst: list[str], ingredients: list[str]) -> int:
 
     return 1
 
 def process_input(input_file):
-    pass
+    """
+    Parse input file to capture fresh ingredient ranges and available
+    ingredients into two separate lists and return them.
+    """
+    range_lst = []
+    ingredients = []
+
+    with open(input_file, 'r', encoding='utf-8') as f:
+        for line in f:
+            if re.match(r"^\d+[-]\d+$", line):
+                range_lst.append(line.strip())
+            elif re.match(r"^\d+$", line):
+                ingredients.append(line.strip())
+
+    # print(range_lst)
+    # print("--------BREAK---------")
+    # print(ingredients)
+
+    return range_lst, ingredients
 
 def main():
-    pass
+    process_input("test_input.txt")
 
 if __name__ == '__main__':
     main()
--- day_05/test_fresh_finder.py
+++ day_05/test_fresh_finder.py
@@ -1,5 +1,12 @@
 import pytest
 from fresh_finder import find_fresh
+from fresh_finder import process_input
+
+def test_process_input():
+    range_lst, ingred_lst = process_input("test_input.txt")
+
+    assert range_lst == ["3-5", "10-14", "16-20", "12-18"]
+    assert ingred_lst == ["1", "5", "8", "11", "17", "32"]
 
 def test_find_fresh():
     ingredient_ranges = ["3-5", "10-14", "16-20", "12-18"]
